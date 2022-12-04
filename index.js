043b502853474d2981ad846d9261e7110b31a551
Lela Deslauriers <lela.deslauriers@gmail.com>
2022-12-04 15:17:08 -0800

    commit tree e635783819a244c382596b2f0b80e2efdddb4b4f

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);//appends a cat to the end of the cats array
+    console.log(cats);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);//prepends a cat to the beginning of the cats array
+    console.log(cats);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();//removes the last cat from the cats array
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();//removes the first cat from the cats array
+}
+
+function appendCat(name) {
+    appendCat = [...cats, name];
+    return appendCat;
+}
+
+function prependCat(name) {
+    appendCat = [name, ...cats];
+    return appendCat;
+}
+
+function removeLastCat() {
+    removeLastCat = cats.slice(0, cats.length -1);
+    return removeLastCat;
+    //removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
+}
+
+function removeFirstCat() {
+    removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+    //removes the first cat from the cats array and returns a new array, leaving the cats array unchanged
+}
